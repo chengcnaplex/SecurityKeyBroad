83ba00d2aad33862ae9f6190c76e872bc68695dc
chengcnaplex <chengcn_aplex@aliyun.com>
2016-07-01 10:28:18 +0800

    "first version of securitykeybroad"

--- app/src/main/java/com/example/chengmengzhen/securitykeybroad/MainActivity.java
+++ app/src/main/java/com/example/chengmengzhen/securitykeybroad/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.chengmengzhen.securitykeybroad;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.chengmengzhen.securitykeybroad.view.SercurityDialog;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void click(View view){
+        SercurityDialog dialog = new SercurityDialog(this);
+        dialog.show();
     }
 }
---
+++ app/src/main/java/com/example/chengmengzhen/securitykeybroad/view/SercurityDialog.java
@@ -0,0 +1,47 @@
+package com.example.chengmengzhen.securitykeybroad.view;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Gravity;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.example.chengmengzhen.securitykeybroad.R;
+
+/**
+ * Created by chengmengzhen on 16/6/30.
+ */
+public class SercurityDialog extends Dialog{
+    private View mRootView;
+    public SercurityDialog(Context context) {
+        super(context, R.style.SercurityDialogTheme);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //给dialog设置布局
+        setContentView(R.layout.ui_security_dialog);
+        //通过window设置获取dialog参数
+        Window window = getWindow();
+        WindowManager.LayoutParams params = window.getAttributes();
+
+        //获取屏幕的宽高
+        WindowManager manager = (WindowManager) getContext()
+                .getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics outMetrics = new DisplayMetrics();
+        manager.getDefaultDisplay().getMetrics(outMetrics);
+        int width = outMetrics.widthPixels;
+        int height = outMetrics.heightPixels;
+
+        //设置dialog的宽
+        params.width = width;
+        //设置dialog在屏幕中的位置
+        params.gravity = Gravity.BOTTOM|Gravity.CENTER_HORIZONTAL;
+        //设置dialog属性
+        window.setAttributes(params);
+    }
+}
