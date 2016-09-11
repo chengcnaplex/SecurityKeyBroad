499381f928b9dabacc28d94c91b37f1d208dc04f
zengjf <zengjf42@163.com>
2016-09-11 13:09:19 +0800

    删除一些无效文件，删除无效代码，重构SercurityDialog.java文件内容，修改gif图片链接地址。

--- app/src/main/java/com/example/chengmengzhen/securitykeybroad/MainActivity.java
+++ app/src/main/java/com/example/chengmengzhen/securitykeybroad/MainActivity.java
@@ -20,21 +20,35 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    /**
+     * 这个click函数在activity_main中有指定，相当于重写了onClick()方法。
+     *
+     * @param view
+     */
     public void click(View view) {
+        // 创建显示SercurityDialog对话框，也就是输入密码对话框
         SercurityDialog dialog = new SercurityDialog(this);
         dialog.setOnInputCompleteListener(new SercurityDialog.InputCompleteListener() {
+
+            /**
+             * 当密码输入完成的时候，这个函数会被回调
+             *
+             * @param PassWord
+             */
             @Override
             public void inputComplete(String PassWord) {
+
                 //把password转为byte数组
                 String[] pwdStrings = PassWord.replace("[", "").replace("]", "").split(",");
                 byte[] PwdBytes = new byte[pwdStrings.length];
                 for (int i = 0; i < PwdBytes.length; i++) {
                     PwdBytes[i] = Byte.valueOf(pwdStrings[i]);
                 }
+
                 //jni加密
                 String enPwd = JniEncode.encryption(PwdBytes);
 
-                //加密后的byte数组
+                //加密后转为byte数组
                 String[] split = enPwd.replace("[", "").replace("]", "").split(",");
                 byte[] enPwdBytes = new byte[split.length];
                 for (int i = 0; i < enPwdBytes.length; i++) {
